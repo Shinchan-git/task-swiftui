3d7dd33af8efe1ca86e8a36c6c6fe5dfe9d37f74
Shinchan-git <gyoshu.bowie@gmail.com>
2022-02-27 21:22:04 +0900

    Initial Commit

---
+++ TaskSwiftUIApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TaskSwiftUIApp
+//
+//  Created by Owner on 2022/02/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TaskSwiftUIApp/TaskSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskSwiftUIAppApp.swift
+//  TaskSwiftUIApp
+//
+//  Created by Owner on 2022/02/27.
+//
+
+import SwiftUI
+
+@main
+struct TaskSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
